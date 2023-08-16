0cba7e5bffcfa3ebcecb291c0d0ff00f7bbbc618
Michael Harrington <kibiz0r@gmail.com>
2023-08-16 15:41:01 -0400

    sb init

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,12 +1,4 @@
 module.exports = {
-  stories: ["../components/**/*.stories.?(ts|tsx|js|jsx)"],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-react-native-web",
-  ],
-  framework: "@storybook/react",
-  core: {
-    builder: 'webpack5',
-  },
+  stories: ['./stories/**/*.stories.?(ts|tsx|js|jsx)'],
+  addons: ['@storybook/addon-ondevice-controls', '@storybook/addon-ondevice-actions'],
 };
